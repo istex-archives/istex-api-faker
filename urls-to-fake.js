7403dd337fb438b89b114e6f336a6142d9d9ed18
Stéphane Gully <stephane.gully@gmail.com>
2015-02-18 15:52:09 +0100

    ade jsonp data

--- urls-to-fake.js
+++ urls-to-fake.js
@@ -51,6 +51,16 @@ urls.push({
 });
 
 urls.push({
+  filename: 'document_jsonp_q_eq_star_and_output_eq_star_and_size_eq_10_and_from_eq_0_and_stats_eq_1.json',
+
+  protocol: 'https',
+  hostname: 'api.istex.fr',
+  pathname: '/document/',
+  query: { callback: '_jqjsp', q: 'brain', output: '*', size: '10', from: '0', stats: '1' }
+});
+
+
+urls.push({
   filename: 'document_q_eq_star_and_output_eq_star_and_size_eq_10_and_from_eq_10_and_stats_eq_1.json',
 
   protocol: 'https',
